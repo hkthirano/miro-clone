2df3f7d45824a2e84aa2bfee52a5a1ffc59c04cf
hkthirano <hirano.hkt@gmail.com>
2024-06-13 19:54:03 +0900

    ボードカードにコピーと削除を追加

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -6,7 +6,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { formatDistanceToNow } from "date-fns";
 
+import { MoreHorizontal } from "lucide-react"
+
 import { Skeleton } from "@/components/ui/skeleton";
+import { Actions } from "@/components/actions"
 
 import { Overlay } from "./overlay";
 import { Footer } from "./footer"
@@ -48,6 +51,17 @@ export const BoardCard = ({
                         fill
                         className="object-fill" />
                     <Overlay />
+                    <Actions
+                        id={id}
+                        title={title}
+                        side="right"
+                    >
+                        <button
+                            className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none" >
+                            <MoreHorizontal
+                                className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+                        </button>
+                    </Actions>
                 </div>
                 <Footer
                     isFavorite={isFavorite}
